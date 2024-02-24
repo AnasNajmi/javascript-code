15cdd3ae7439a92bdf0fdcd0e83d0a141ba9b426
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-02-24 16:56:42 +0000

    Learn about const, let and console.table([])

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const studentId = 1234
+let studentName = "Anas" 
+let StudentAddress = "london"
+let StudentEmail = "Pakistan@gmail.com"
+let Studentphone = "1234567"
+console.table([studentId, studentName, StudentAddress, StudentEmail, Studentphone])
+console.log(studentName);
+//do not use var to declare variable because it is an old pratice//
+
+
+
