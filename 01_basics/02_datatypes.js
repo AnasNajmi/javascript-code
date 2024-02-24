c8e18585a4d354c0580365c2b0a2159f5e1bec8a
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-02-24 17:37:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version//
+//alert(3+3)// we are using nodejs, not browser
+console.log( 3 + 3)
+//mdn website is best for the documentation of JS//
+
+let name = "anas"  //String => ""//
+
+let age = 18 //Number/int => 2 raise to power 53//
+
+let isloggedin = true //boolean => true/false
+
+//null => standalone value// null is an =>object<= important interview question//
+
+//undefined => value not assign//
+
+//bigint => for a very long integer value//
+
+//symbol => unique mostly used while using react//
+
+//object => will discuss in further lectures//
+
+console.log(typeof 123)
+console.log(typeof null);
