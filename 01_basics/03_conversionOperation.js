2d9fab3a0d048bf4021a67a5c8099497e6da93bb
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-02-24 18:58:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "33"
+let example = null
+let valueinexample = Number(example)
+let valueInnumber = String(score)
+let num = "anas"
+let numb = Number(num)
+let anasn = null
+let anasna = Number(anasn)
+let usa = undefined
+let usam = Number(usa)
+let mun = true
+let munu = Number(mun)
+let IsLoggedIn = 1 // 1 => true, 0 =>false,  "" => false, "anas"=>true 
+let booleanisloggedin = Boolean(IsLoggedIn)
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+console.log(booleanisloggedin);
+console.log(typeof mun)
+console.log(munu);
+console.log(usam)
+console.log(typeof usa);
+console.log(typeof anasna)
+console.log(anasna)
+console.log(typeof score)
+console.log(typeof valueInnumber)
+console.log(typeof example)
+console.log(typeof valueinexample)
+console.log(typeof numb);
+console.log(typeof num)
