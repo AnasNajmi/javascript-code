891266d7556e819fe6a4099b2b65fac5ffb17a86
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-02-25 16:24:53 +0000

    important math methods along with math.random method

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,19 @@
+const score = 500
+const balance = new Number(900)
+const number = 1000000
+const newnumber = 25.89888
+// console.log(newnumber.toPrecision(6));
+// console.log(balance)
+// console.log(score);
+// console.log(typeof balance.toString());// Converts the number into string
+// console.log(balance.valueOf())
+// console.log(number.toLocaleString('en-IN'));// Converts the number into Indian number system
+
+// +++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++
+// Math Methods
+// console.log(Math.PI);// Displays the value of PI
+// console.log(Math.max(100, 23, 45, 65, 100000));// selects and displays the Largest number
+// console.log(Math.abs(-56)); //converts negative value into positive value
+let max = 20
+let min = 10
+console.log(Math.floor(Math.random()*(max - min + 1) +10));
