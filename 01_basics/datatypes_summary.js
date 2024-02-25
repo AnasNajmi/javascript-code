ebec6cf4fa15ff50dfa66e448a9e5b43f3e0f1ed
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-02-25 07:00:21 +0000

    important for interview, Stack and Heap memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -35,3 +35,29 @@ let myobj = {
     console.log("hello world")
  }
 console.log(typeof myfunction);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Stack and Heap Memory in Javascript
+//Stack(Primtive datatype) Jo bhi varibale declare kia hy humy uski aikcopy milti hy
+//Heap(Non-Primitive) Original value ka reference milta hy
+
+//Heap Example
+let userone = {
+    name: "anas",
+    email: "anas@gmail.com",
+}
+
+let usertwo = userone
+usertwo.email = "usamanajmi@gmail.com"
+console.log(userone);
+console.log(usertwo);
+console.log(usertwo.email);
+
+//Stack Example 
+let num1 = "1234"
+let num2 = num1
+num2 = "456"
+console.log(num1);
+console.log(num2)
+
+
