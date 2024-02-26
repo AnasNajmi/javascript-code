d5584937db8aa662a2ac347e1dbd335fb9e0dbcf
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-02-26 18:31:26 +0000

    Array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+const arr = [1, 2, 3, 4, 5,]
+//This method adds value to the array
+// arr.push(7)
+//This method POPS the last value of the array
+// arr.pop()
+
+arr.shift() //it removes the first digit of the array 
+arr.unshift(2) // It adds the given value at the start of the array 
+
+console.log(arr.includes(2))
+console.log(arr.indexOf(5));
+
+
+console.log(arr)
