fdf7349fdecd6cf783451542d074f951f896ea90
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-02-27 06:22:10 +0000

    very important techniques related to OBJECTS

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -14,10 +14,20 @@ let myobj = {
 }
 //There are 2 ways to acess the value of an object and that is explained below
 
-console.log(myobj.IsLoggedin);// First method
-console.log(myobj["Age"]);// Second Method
-console.log(myobj[mysym]);
+// console.log(myobj.IsLoggedin);// First method
+// console.log(myobj["Age"]);// Second Method
+// console.log(myobj[mysym]);// This is a special case to excess the Symbol defined inside the object
+// console.log(myobj);
 
-console.log(myobj);
-myobj.email = "anas@gmailchatgpt.com" //we can change the value of an object by using the "=" symbol 
-console.log(myobj);
+// myobj.email = "anas@gmailchatgpt.com" //we can change the value of an object by using the "=" symbol 
+// console.log(myobj);
+
+//Method to define an object inside a Function 
+myobj.greeting = function(){
+    console.log("Hello JS user");
+}
+myobj.greetingtwo = function(){
+    console.log(`Hello JS user", ${this.name}`);// string interpolation
+}
+console.log(myobj.greeting());
+console.log(myobj.greetingtwo());
