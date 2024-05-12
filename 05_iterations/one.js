77a17f4a72e3d564272d1a2ea2138f34f1198e9c
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-05-12 12:27:43 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+
+
+
+for (let index = 0; index !=20 ; index++) {
+    const element = index;
+    if(index==5){
+        // console.log("5 is the best number ever");
+    }
+    // console.log(element);
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`outer loop value ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(i+ '*' + j + '=' + i*j );
+        
+        
+    }
+    
+}
