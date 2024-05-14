1562b2dbc81a10df67bb1005fb1f1de5e0741cdb
AnasNajmi <127054270+AnasNajmi@users.noreply.github.com>
2024-05-14 04:58:43 +0000

    solved problems

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -4,16 +4,16 @@
 for (let index = 0; index !=20 ; index= index + 2) {
     const element = index;
     if(index==5){
-         console.log("5 is the best number ever");
+        //  console.log("5 is the best number ever");
     }
-     console.log(element);
+    //  console.log(element);
     
 }
 
 for (let i = 1; i <= 10; i++) {
-    console.log(`outer loop value ${i}`);
+    // console.log(`outer loop value ${i}`);
     for (let j = 1; j <= 10; j++) {
-        console.log(i+ '*' + j + '=' + i*j );
+        // console.log(i+ '*' + j + '=' + i*j );
         
         
     }
@@ -21,32 +21,40 @@ for (let i = 1; i <= 10; i++) {
 }
 
 let myarray = ["batman", "aquaman", "spiderman"]
-console.log(myarray.length);
+// console.log(myarray.length);
 for (let i = 0; i < myarray.length; i++) {
     const element = myarray[i];
-    console.log(element);
+    // console.log(element);
     
     
 }
 
 for (let index = 1; index <= 20; index++) { ///BREAK CONDITION
     if(index==5){
-        console.log(`Detected 5`);
+        // console.log(`Detected 5`);
         break
     }
-    console.log(`value of index is ${index}`);
+    // console.log(`value of index is ${index}`);
     
 }
 
 for (let index = 1; index <= 20; index++) { /// CONTINUE CONDITION
     if(index==5){
-        console.log(`Detected 5`);
+        // console.log(`Detected 5`);
         continue
     }
-    console.log(`value of index is ${index}`);
+    // console.log(`value of index is ${index}`);
     
 }
 
+for (let index = 0; index <=20; index++) { //Checks for Even and Odd numbers 
+    if(index % 2===0){
+        console.log(`${index} is an Even Number`);
+    }else{
+        console.log(`${index} is an Odd Number`);
+    }
+    // console.log(index);
+}
 
 
 
